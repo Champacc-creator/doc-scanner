bd3b948cd196d0aca4173b7d963d15a5c3ff87b0
Champacc-creator <sittiwat.acc@gmail.com>
2026-09-08 11:51:28 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const API_KEY = 'MY_SECRET_KEY_2569';
 
 async function callAPI(action, payload = {}) {
   const res = await fetch(API_URL, {
-    method: 'POST',
+    method: 'GET',
     // ต้องใช้ text/plain เพื่อเลี่ยง CORS preflight ของ Apps Script
     headers: { 'Content-Type': 'text/plain;charset=utf-8' },
     body: JSON.stringify({ action, key: API_KEY, ...payload })
