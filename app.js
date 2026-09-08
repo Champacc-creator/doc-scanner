76d0a13efe77c64888250265e41a885e78a7e67c
Champacc-creator <sittiwat.acc@gmail.com>
2026-09-08 11:19:27 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://script.google.com/macros/s/1z1xAFs0flOi4-WmHkvaI1wuhWcsguNU51i3qyGGnAkk/exec'; // ← ลิงก์จากขั้นที่ 2
+const API_URL = 'https://script.google.com/macros/s/AKfycbyR9Mv0WCXysPKgIIzB_STi5rQlGAz28rfSQ5k2-E8oDzrEFe4fMYYk-Z8tMJX5S2eb/exec'; // ← ลิงก์จากขั้นที่ 2
 const API_KEY = 'MY_SECRET_KEY_2569';
 
 async function callAPI(action, payload = {}) {
